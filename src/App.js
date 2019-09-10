2596cc972d5ff4c698b0b0ff43d2df419dafa3fe
JoelVerastegui <joelverastegui99@gmail.com>
2019-09-09 22:44:38 -0500

    Implement create hero

--- src/App.js
+++ src/App.js
@@ -8,9 +8,37 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      players
+      players,
+      player: {
+        name: '',
+        hp: 0,
+        mp: 0,
+        kills: 0
+      }
     }
+    this.createHero = this.createHero.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
+
+  createHero(event) {
+    event.preventDefault();
+    this.setState({
+      players: [...this.state.players, this.state.player]
+    })
+  }
+
+  handleInput(e) {
+    e.preventDefault();
+    let { name, value } = e.target;
+    this.setState(prevState => ({
+      player: {
+        ...prevState.player,
+        [name]: value
+      }
+    })
+    )
+  }
+
   render() {
     const playerList = this.state.players.map((player, i) => {
       return (
@@ -32,7 +60,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="navbar shadow bg-danger text-white d-block text-center h5">
-          Sebas se la come doblada y entera, SAPEEEEEEEEEEEEEEE
+          First Blood
         </div>
         <div className="container">
           <div className="row">
@@ -42,14 +70,26 @@ class App extends React.Component {
                   New Hero
                 </div>
                 <div className="card-body">
-                  <form>
+                  <form className="text-left" onSubmit={this.createHero}>
                     <div className="form-group">
-                      <label for="name">Name</label>
-                      <input id="name" className="form-control" placeholder="Name"></input>
+                      <label>Name</label>
+                      <input name="name" className="form-control" placeholder="Name" onChange={this.handleInput}></input>
+                      <small className="form-text text-muted">Enter a cool name!</small>
                     </div>
                     <div className="form-group">
-                      <label for="hp">HP</label>
-                      <input id="hp" className="form-control" placeholder="HP"></input>
+                      <label>HP</label>
+                      <input name="hp" className="form-control" placeholder="HP" onChange={this.handleInput}></input>
+                    </div>
+                    <div className="form-group">
+                      <label>MP</label>
+                      <input name="mp" className="form-control" placeholder="MP" onChange={this.handleInput}></input>
+                    </div>
+                    <div className="form-group">
+                      <label>Kills</label>
+                      <input name="kills" className="form-control" placeholder="Kills" onChange={this.handleInput}></input>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                      <button type="submit" className="btn btn-success">Create</button>
                     </div>
                   </form>
                 </div>
